6616228cba51428de59352a5e424c413e54e904c
David de Leeuw <00276113@rijnijssel.nl>
2018-03-28 20:53:16 +0200

    iets

--- Web/includes/adminmenu.inc.php
+++ Web/includes/adminmenu.inc.php
@@ -1,16 +1,18 @@
 <div class="row">
     <div class="col s4" style="text-align:center;">
-        <a target="_blank" href="">
+        <a href="?page=medewerkerbeheer">
             <img src="images/medewerkers.png" alt="" width="150" height="150">
         </a>
         <div class="desc">Medewerkers</div>
     </div>
+
     <div class="col s4" style="text-align:center;">
-        <a target="_blank" href="">
+        <a href="?page=expertisegroepen">
             <img src="images/expertisegroepen.png" alt="" width="150" height="150">
         </a>
         <div class="desc">Expertisegroepen</div>
     </div>
+
     <div class="col s4" style="text-align:center;">
         <a target="_blank" href="">
             <img src="images/projecten.png" alt="" width="150" height="150">
@@ -18,6 +20,7 @@
         <div class="desc">Projecten</div>
     </div>
 </div>
+
 
 <div class="row">
     <div class="col s4" style="text-align:center;">
