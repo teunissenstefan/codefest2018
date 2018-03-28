2587626335547ab6b1e54eef6a9f8b67ad3d3bd7
Thomas van Minnen <thomasvanminnen@gmail.com>
2018-03-28 11:37:00 +0200

    Afbeeldingen toegevoegd

---
+++ Web/includes/login.inc.php
@@ -0,0 +1,14 @@
+<div class="container">
+
+
+<div class="login">
+<form action="/action_page.php">
+Gebruikersnaam:<br>
+<input type="text" name="uname" value=""><br>
+Wachtwoord:<br>
+<input type="password" name="pass" value="" maxlength="50"><br>
+<input type="submit" value="Login">
+</form>
+</div>
+
+</div>
