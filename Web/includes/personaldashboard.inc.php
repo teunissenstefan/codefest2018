8889b5fa685ddd3abba5c3092ea6bcfcfc0adbf6
Nino Perez Vazquez <n.perez@hotmail.nl>
2018-03-29 04:55:52 +0200

    buttons

--- Web/includes/personaldashboard.inc.php
+++ Web/includes/personaldashboard.inc.php
@@ -1,5 +1,20 @@
-<div class="row">
-    <div class="col l4">
-        <a href="?page=mijnskills"><button>Mijn skills</button></a>
-    </div>
+<img src="images/profile.png" width="240px" height="200px">
+<div class="col l4">
+	<a href="?page=mijnskills"><button>Mijn skills</button></a>
+</div>
+<br>
+<div class="col l4">
+	<a href="?page=mijninzet"><button>Mijn inzet</button></a>
+</div>
+<br>
+<div class="col l4">
+	<a href="?page=medewerkers"><button>Medewerkers</button></a>
+</div>
+<br>
+<div class="col l4">
+	<a href="?page=expertisegroepen-u"><button>Expertisegroepen</button></a>
+</div>
+<br>
+<div class="col l4">
+	<a href="?page=projecten"><button>Projecten</button></a>
 </div>
